08b1edc7e164c08ffac30f89ac695906058dd2e0
Jacob Kjeldahl <jkj@lenio.dk>
2009-04-28 13:09:47 +0200

    Added missing methods and removed methods not needed

--- lib/mysql_schema_bulk_change/mysql_adapter.rb
+++ lib/mysql_schema_bulk_change/mysql_adapter.rb
@@ -14,22 +14,14 @@ module ActiveRecord
       end
       
       def add_column(table_name, column_name, type, options = {})
-        raise "Tablename mismatch expected '#{@table_name}', but got '#{table_name}'" unless @table_name == table_name
-        column(column_name, type, options)
-      end
-      
-      def column(column_name, type, options = {})
+        check_table_name(table_name)
         alter_specification = "ADD #{@base.quote_column_name(column_name)} #{@base.type_to_sql(type, options[:limit], options[:precision], options[:scale])}"
         @base.add_column_options!(alter_specification, options)
         add_stmt(alter_specification)
       end
       
-      def add_index(table_name, column_name, options)
-        raise "Tablename mismatch expected '#{@table_name}', but got '#{table_name}'" unless @table_name == table_name
-        index(column_name, options)
-      end
-      
-      def index(column_name, options = {})
+      def add_index(table_name, column_name, options = {})
+        check_table_name(table_name)
         column_names = Array(column_name)
         index_name   = @base.index_name(@table_name, :column => column_names)
 
@@ -42,43 +34,70 @@ module ActiveRecord
         quoted_column_names = column_names.map { |e| @base.quote_column_name(e) }.join(", ")
         add_stmt("ADD #{index_type} INDEX #{@base.quote_column_name(index_name)}(#{quoted_column_names})")
       end
-       
-      def remove(*column_names)
+      
+      def add_timestamps(table_name)
+        check_table_name(table_name)
+        add_column table_name, :created_at, :datetime
+        add_column table_name, :updated_at, :datetime
+      end
+      
+      def remove_timestamps(table_name)
+        remove_column table_name, :updated_at
+        remove_column table_name, :created_at
+      end
+      
+      def change_column(table_name, column_name, type, options = {})
+        check_table_name(table_name)
+        column = @base.column_for(@table_name, column_name)
+
+        unless @base.options_include_default?(options)
+          options[:default] = column.default
+        end
+
+        unless options.has_key?(:null)
+          options[:null] = column.null
+        end
+
+        change_column_sql = "CHANGE #{@base.quote_column_name(column_name)} #{@base.quote_column_name(column_name)} #{@base.type_to_sql(type, options[:limit], options[:precision], options[:scale])}"
+        @base.add_column_options!(change_column_sql, options)
+        add_stmt(change_column_sql)
+      end
+
+      def change_column_default(table_name, column_name, default) #:nodoc:
+        check_table_name(table_name)
+        column = @base.column_for(@table_name, column_name)
+        change_column(@table_name, column_name, column.sql_type, :default => default)
+      end
+
+      def rename_column(table_name, column_name, new_column_name) #:nodoc:
+        check_table_name(table_name)
+        options = {}
+        if column = @base.columns(table_name).find { |c| c.name == column_name.to_s }
+          options[:default] = column.default
+          options[:null] = column.null
+        else
+          raise ActiveRecordError, "No such column: #{table_name}.#{column_name}"
+        end
+        current_type = @base.select_one("SHOW COLUMNS FROM #{quote_table_name(table_name)} LIKE '#{column_name}'")["Type"]
+        rename_column_sql = "CHANGE #{quote_column_name(column_name)} #{quote_column_name(new_column_name)} #{current_type}"
+        @base.add_column_options!(rename_column_sql, options)
+        add_stmt(rename_column_sql)
+      end
+
+      def remove_column(table_name, *column_names)
         add_stmts(column_names.flatten.map { |column_name| "DROP COLUMN #{@base.quote_column_name(column_name)}"})
       end
+      alias_method :remove_columns, :remove_column
       
       def remove_index(table_name, options = {})
-        raise "Tablename mismatch expected '#{@table_name}', but got '#{table_name}'" unless @table_name == table_name
+        check_table_name(table_name)
         add_stmt("DROP INDEX #{@base.quote_column_name(@base.index_name(@table_name, options))}")
       end
-            
-      # Adds a column or columns of a specified type
-      # ===== Examples
-      #  t.string(:goat)
-      #  t.string(:goat, :sheep)
-      %w(string text integer float decimal datetime timestamp time date binary boolean ).each do |column_type|
-        class_eval %{
-          def #{column_type}(*args)                                          # def string(*args)
-            options = args.extract_options!                                  #   options = args.extract_options!
-            column_names = args                                              #   column_names = args
-                                                                             #
-            column_names.each do |name|                                      #   column_names.each do |name|
-              column = ColumnDefinition.new(@base, name, '#{column_type}')   #     column = ColumnDefinition.new(@base, name, 'string')
-              if options[:limit]                                             #     if options[:limit]
-                column.limit = options[:limit]                               #       column.limit = options[:limit]
-              elsif self.native['#{column_type}'.to_sym].is_a?(Hash)         #     elsif native['string'.to_sym].is_a?(Hash)
-                column.limit = self.native['#{column_type}'.to_sym][:limit]  #       column.limit = native['string'.to_sym][:limit]
-              end                                                            #     end
-              column.precision = options[:precision]                         #     column.precision = options[:precision]
-              column.scale = options[:scale]                                 #     column.scale = options[:scale]
-              column.default = options[:default]                             #     column.default = options[:default]
-              column.null = options[:null]                                   #     column.null = options[:null]
-              column(name, column.sql_type, options)                         #     column(name, column.sql_type, options)
-            end                                                              #   end
-          end                                                                # end
-        } , __FILE__, __LINE__
-      end
-      
+       
+       def type_to_sql(type, limit = nil, precision = nil, scale = nil)
+         @base.type_to_sql(type, limit, precision, scale)
+       end
+                  
       # Commits the updates to the database and clears the cached statements
       def update_database
         unless @statements.empty?
@@ -92,17 +111,19 @@ module ActiveRecord
       def native
         @base.native_database_types
       end
+      alias_method :native_database_types, :native
        
       private
+      def check_table_name(table_name)
+        raise "Tablename mismatch expected '#{@table_name}', but got '#{table_name}'" unless @table_name == table_name
+      end
+      
       def add_stmt(*stmt)
-        @statements + stmt.flatten
+        @statements += stmt.flatten
       end
       alias :add_stmts :add_stmt
       
       def method_missing(symbol, *args)
-        #puts "Missing: #{symbol} called from \n#{Kernel.caller.join("\n")}"
-        update_database # Need to flush the cache in case some of the cached columns are required in the
-                        # SQL about to be executed
         @base.send symbol, *args
       end
       
@@ -110,11 +131,12 @@ module ActiveRecord
     
     class MysqlAdapter < AbstractAdapter
       # Changed to return a proxy for collecting the changes and do bulk updates
+      alias_method :change_table_direct, :change_table
       def change_table(table_name)
         bulk_updater = MysqlBulkProxy.new(table_name, self)
         yield Table.new(table_name, bulk_updater)
         bulk_updater.update_database
-      end      
+      end  
     end
   end
 end
--- spec/mysql_schema_bulk_change_spec.rb
+++ spec/mysql_schema_bulk_change_spec.rb
@@ -1,39 +1,40 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-# Time to add your specs!
-# http://rspec.info/
 describe 'mysql_schema_bulk_change' do
+  def connection
+    ActiveRecord::Base.connection
+  end
+  
   before(:each) do
       ActiveRecord::Migration.verbose = true
       begin
-        ActiveRecord::Base.connection.tables.each do |table|
-          ActiveRecord::Base.connection.drop_table(table)
+        connection.tables.each do |table|
+          connection.drop_table(table)
         end
       rescue
       end
-      ActiveRecord::Base.connection.create_table :test_table do |t|
+      connection.create_table :test_table do |t|
         t.string :name
       end
-      ActiveRecord::Base.connection.add_index :test_table, :name
+      connection.add_index :test_table, :name
   end
   after(:each) do
-    #ActiveRecord::Base.connection.initialize_schema_migrations_table
-    #ActiveRecord::Base.connection.execute "DELETE FROM #{ActiveRecord::Migrator.schema_migrations_table_name}"
     begin
-      ActiveRecord::Base.connection.tables.each do |table|
-        ActiveRecord::Base.connection.drop_table(table)
+      connection.tables.each do |table|
+        connection.drop_table(table)
       end
     rescue
     end
   end 
   
   it "should not call the connection adapter in the block" do
-    ActiveRecord::Base.connection.should_not_receive(:add_column)    
-    ActiveRecord::Base.connection.should_not_receive(:add_index)
-    ActiveRecord::Base.connection.should_receive(:execute)
-    ActiveRecord::Base.connection.change_table :test_table do |t|
+    connection.should_not_receive(:add_column)    
+    connection.should_not_receive(:add_index)
+    connection.should_receive(:execute).with("ALTER TABLE `test_table` ADD `author` varchar(255), ADD  INDEX `index_test_table_on_author`(`author`), DROP INDEX `index_test_table_on_name`")
+    connection.change_table :test_table do |t|
       t.string :author
       t.index :author
+      t.remove_index :name
     end
   end
   
