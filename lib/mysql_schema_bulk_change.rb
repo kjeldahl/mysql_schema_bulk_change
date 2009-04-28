509583dcdfe792986ec1704fce803e88a1403c39
Jacob Kjeldahl <jkj@lenio.dk>
2009-04-28 13:20:43 +0200

    Version bump

--- lib/mysql_schema_bulk_change.rb
+++ lib/mysql_schema_bulk_change.rb
@@ -2,7 +2,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module MysqlSchemaBulkChange
-  VERSION = '0.1.0'
+  VERSION = '0.2.0'
 end
 
 require 'mysql_schema_bulk_change/mysql_adapter'
--- mysql_schema_bulk_change.gemspec
+++ mysql_schema_bulk_change.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |s|
   s.name = %q{mysql_schema_bulk_change}
-  s.version = "0.1.0"
+  s.version = "0.2.0"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Jacob Kjeldahl"]
